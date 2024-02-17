70ed6bb92886850a42b922d29b71c8ddcca6720b
mainadev <62764842+devmaina@users.noreply.github.com>
2024-02-17 17:12:10 +0300

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Welcome to Safe Submit</h1>
+    <p>A simple solution for hosting submission messages without a server.</p>
+    <nuxt-link to="/submissions">View Submissions</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
